b89e545d732cb0a6d51de10f9dd6f15c4a5134d8
Mu-nah <102527479+Mu-nah@users.noreply.github.com>
2026-04-30 07:45:30 +0100

    remove unused files

--- tmp_nba_stats_test.py
+++
@@ -1,4 +0,0 @@
-from multi_sport_bot import fetch_nba_team_season_stats
-
-print('WAS', fetch_nba_team_season_stats(30, '2024-25', 'Washington Wizards'))
-print('GSW', fetch_nba_team_season_stats(10, '2024-25', 'Golden State Warriors'))
--- tmp_test_requests.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-print('ok')
-print('requests', requests.__version__)
-print('httpbin', requests.get('https://httpbin.org/get', timeout=10).status_code)
